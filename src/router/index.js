849641fc1068d6a6e06eb8e17134db1bd69a4fc4
Luciano <lucho.2012.tandil@gmail.com>
2024-01-26 21:39:22 -0300

    Se quitan componentes about y home ya que no serán utilizados en esta versión

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Layout from '../layout/Layout.vue'
+import HomePage from '../views/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,12 +14,9 @@ const router = createRouter({
       children: []
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: "/dashboard",
+      name: "dashboard",
+      component: HomePage,
     }
   ]
 })
